2621664579f268a0d3062933f0b03c3ab2b41d75
Katkov Kirill <katkov.kirill10@gmail.com>
2021-11-15 16:49:37 +0200

    added basic navigation with react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>;   
+  return (   
+      <BrowserRouter>
+        <Switch>         
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />         
+        </Switch>
+      </BrowserRouter>      
+  )  
 }
 
 export default App;
