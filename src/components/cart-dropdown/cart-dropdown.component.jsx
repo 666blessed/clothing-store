6455febef8d72ba050702944bc48a8048a84aecd
Katkov Kirill <katkov.kirill10@gmail.com>
2021-12-20 10:41:19 +0200

    updated checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,8 +6,9 @@ import {connect} from 'react-redux';
 import { selectCartItems } from "../../redux/cart/cart.selectors"; 
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { toggleCardHidden } from '../../redux/cart/cart.actions.js';
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -17,8 +18,11 @@ const CartDropdown = ({cartItems, history}) => (
                 (<span className='empty-message'>Your cart is empty</span>)
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>
-            Go to cheсkout
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCardHidden());
+            }}>
+                Go to cheсkout
         </CustomButton>
     </div>
 )
